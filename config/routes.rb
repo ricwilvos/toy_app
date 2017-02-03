0c7b2e7377a939f83943d1c82332fcd5e6c9e543
Rich <ricwilvos@gmail.com>
2017-02-03 04:04:24 +0000

    Lab 4 complete.  Added nav bar and bootstrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/about'
+
+  get 'home/contact'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'users#index'
+root 'home#index'
 end
